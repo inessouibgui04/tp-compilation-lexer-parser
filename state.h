89b0f6382e8c06e7d0042ee90ca765dfe73f4208
Mm-12324 <malakbahda11@gmail.com>
2026-03-08 09:28:13 +0100

    lexer, automate et tests

--- state.h
+++ state.h
@@ -1,1 +1,19 @@
+#pragma once
 
+#include "symbole.h"
+
+class Automate;
+
+class Etat {
+
+public:
+    Etat(int num) : numero(num) {}
+    virtual ~Etat() {}
+
+    bool transition(Automate & automate, Symbole * s);
+
+    int getNumero() { return numero; }
+
+private:
+    int numero;
+};
